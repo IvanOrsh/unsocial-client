07140e62a0862966a607cef50967921eb17d4c00
IvanOrsh <ksironov@mail.ru>
2023-03-12 15:57:04 +0300

    add: start-post component
    - wired with modal

--- src/app/home/components/start-post/start-post.component.ts
+++ src/app/home/components/start-post/start-post.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ModalController } from '@ionic/angular';
+
+import { ModalComponent } from './modal/modal.component';
+
+// TODO:
 @Component({
   selector: 'app-start-post',
   templateUrl: './start-post.component.html',
   styleUrls: ['./start-post.component.scss'],
 })
-export class StartPostComponent  implements OnInit {
-
-  constructor() { }
+export class StartPostComponent implements OnInit {
+  constructor(public modalController: ModalController) {}
 
   ngOnInit() {}
 
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'my-modal',
+    });
+    await modal.present();
+    const { role } = await modal.onDidDismiss();
+    console.log(7, role);
+  }
 }

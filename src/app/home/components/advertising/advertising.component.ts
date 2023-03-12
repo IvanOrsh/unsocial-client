4126d1460698e01e955b7a4b5559c5025428a8c3
IvanOrsh <ksironov@mail.ru>
2023-03-12 15:25:42 +0300

    add: advertising component

--- src/app/home/components/advertising/advertising.component.ts
+++ src/app/home/components/advertising/advertising.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO:
+
 @Component({
   selector: 'app-advertising',
   templateUrl: './advertising.component.html',
   styleUrls: ['./advertising.component.scss'],
 })
-export class AdvertisingComponent  implements OnInit {
-
-  constructor() { }
+export class AdvertisingComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }

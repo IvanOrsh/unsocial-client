d071e304ebf521e27c402c7a00b152766c8e3a66
IvanOrsh <ksironov@mail.ru>
2023-03-13 15:15:18 +0300

    add: mobile responsiveness

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,29 +3,30 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
-import { HomePage } from './home.page';
-import { HeaderModule } from './components/header/header.module';
-import { StartPostModule } from './components/start-post/start-post.module';
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HomePage } from './home.page';
+import { HeaderComponent } from './components/header/header.component';
+import { PopoverComponent } from './components/header/popover/popover.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { ModalComponent } from './components/start-post/modal/modal.component';
 import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
 import { AdvertisingComponent } from './components/advertising/advertising.component';
 import { AllPostsComponent } from './components/all-posts/all-posts.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule,
-    HeaderModule,
-    StartPostModule,
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
   declarations: [
     HomePage,
+    HeaderComponent,
+    PopoverComponent,
+    StartPostComponent,
+    ModalComponent,
     ProfileSummaryComponent,
     AdvertisingComponent,
     AllPostsComponent,
+    TabsComponent,
   ],
 })
 export class HomePageModule {}
